7aeeda8f8abc04ec97ad0f53f6a10a647c823d66
kautik10111 <64724830+kautik10111@users.noreply.github.com>
2020-05-21 17:00:18 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,28 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+var pos,angle;
+var engine, world;
+var box3;
+var box2,ground;
+function setup(){
+    var canvas = createCanvas(400,400);
+    engine = Engine.create();
+    world = engine.world;
+
+    box2= new Box(240,100,50,100);
+    box3 = new Box(200,300,50,50);
+    ground= new Ground(200,height,400,20);
+}
+
+function draw(){
+    background(0);
+    Engine.update(engine);
+    console.log(box2.body.position.x);
+    console.log(box2.body.angle);
+    console.log(box2.body.position.y);
+    box3.display();
+    box2.display();
+    ground.display();
+   
+}
